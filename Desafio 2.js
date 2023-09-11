0fe6a920d37208dab8edfa8a7bf10575d9b9dbd0
Maria Seadi <142632295+mariaseadi@users.noreply.github.com>
2023-09-11 10:29:13 -0300

    Add files via upload

---
+++ Desafio 2.js
@@ -0,0 +1,22 @@
+// Estrutura de Decisão
+if (saldoFinal < 10){
+    nivel = "Ferro"
+
+} else if (saldoFinal >= 11 && saldoFinal <= 20) {
+    nivel = "Bronze"
+
+} else if (saldoFinal >= 21 && saldoFinal <= 50) {
+    nivel = "Prata"
+
+} else if (saldoFinal >= 51 && saldoFinal <= 80) {
+    nivel = "Ouro"
+
+} else if (saldoFinal >= 81 && saldoFinal <= 90) {
+    nivel = "Diamante"
+
+} else if (saldoFinal >= 91 && saldoFinal <= 100) {
+    nivel = "Lendário"
+
+} else if (saldoFinal >= 101){
+    nivel = "Imortal"
+}
